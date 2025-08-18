234eebc60879474f065fe78879fb684a60434e28
Zoltán Tamás Szabó <zoli.szabok@gmail.com>
2025-08-18 11:28:57 +0200

    Update property details to include area measurement for currently available space

--- resources/views/properties/show.blade.php
+++ resources/views/properties/show.blade.php
@@ -61,9 +61,7 @@
                                 </tr>
                                 <tr>
                                     <td class="font-bold">{{ __('Currently Available') }}:</td>
-                                    <td>{{ $property->jelenleg_kiado }}
-                                        {{ __($property->jelenleg_kiado_addons ?? '') }}
-                                    </td>
+                                    <td>{{ $property->jelenleg_kiado }} m²</td>
                                 </tr>
                                 <tr>
                                     <td class="font-bold">{{ __('Min. Available') }}:</td>
