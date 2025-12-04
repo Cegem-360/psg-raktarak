a0d9d1a235eb9f80a2816816a9eb2c6bb72e29d4
Zoltán Tamás Szabó <zoli.szabok@gmail.com>
2025-12-04 15:19:52 +0100

    Update buildQuery method to filter by agglomeration based on includeAgglomeration flag

--- app/Livewire/ListRentOffices.php
+++ app/Livewire/ListRentOffices.php
@@ -153,8 +153,8 @@ final class ListRentOffices extends Component
         $query->when($this->min_rent, fn ($query) => $query->where('min_berleti_idoszak', $this->min_rent));
         $query->when($this->min_rent_addons, fn ($query) => $query->where('min_rent_addons', $this->min_rent_addons));
         $query->when(
-            ! $this->includeAgglomeration,
-            fn ($q) => $q->budapestOnly()
+            $this->includeAgglomeration,
+            fn ($q) => $q->agglomeration(), // true => agglomeráció only
         );
         $query->when($this->search, fn ($query) => $query->searchText($this->search));
         $query->when($this->officeName, fn ($query) => $query->byOfficeName($this->officeName));
